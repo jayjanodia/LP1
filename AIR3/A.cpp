738f0e139273ba80a967487c928297e80280218b
jayjanodia <35676384+jayjanodia@users.noreply.github.com>
2019-09-24 11:48:59 +0530

    Incomplete

---
+++ AIR3/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ROW 9
+#define COL 10
+
+//creating a shortcut for int, int pair type
+typedef pair<int,int> Pair;
+
+//creating a shortcut for pair<int, pair<int,int>> type
+typedef pair<double, pair<int,int>> pPair;
---
+++ AIR3/eight_puzzle.py
@@ -0,0 +1,114 @@
+'''source link: https://blog.goodaudience.com/solving-8-puzzle-using-a-algorithm-7b509c331288'''
+
+class Node():
+	'''A node class for A* path finding'''
+	def __init__(self, data, level, fval):
+		'''Initialize the node with the data, level of the node and the calculated fvalue'''
+		self.data = data
+		self.level = level
+		self.fval = fval
+		
+	def generate_child(self):
+		'''Generate child nodes from the given node by moving the blank space in either direction(up,down,left,right)'''
+		x, y = self.find(self.data, '_')
+		val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]] #val_list contains position values for moving blank space in (up, down, left, right resp.
+		children = []
+		for i in val_list:
+			child = self.shuffle(self.data, x, y, i[0], i[1])
+			if child is not None:
+				child_node = Node(child,self.level+1,0)
+				children.append(child_node)
+			return children
+		
+	def shuffle(self,puz, x1, y1, x2, y2):
+		'''Move the blank space in the given direction and if the position values are out of limits then return None'''
+		if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
+			temp_puz = []
+			temp_puz = self.copy(puz)
+			temp = temp_puz[x2][y2]
+			temp_puz[x2][y2] = temp_puz[x1][y1]
+			temp_puz[x1][y1] = temp
+			return temp_puz
+		else:
+			return None
+				
+	def copy(self,root):
+		'''copy function to create a similar matrix of the given node'''
+		temp = []
+		for i in root:
+			t = []
+			for j in i:
+				t.append(j)
+			temp.append(t)
+		return temp
+		
+	def find(self,puz,x):
+		'''Used to find position of the blank space'''
+		for i in range(len(self.data)):
+			for j in range(len(self.data)):
+				if puz[i][j] == x:
+					return i,j
+
+class Puzzle:
+	def __init__(self,size):
+		'''Initialize the puzzle size by the specified size, open and closed lists are empty'''
+		self.n = size
+		self.open = []
+		self.closed = []
+
+	def accept(self):
+		'''takes input from the user'''
+		puz = []
+		for i in range(self.n):
+			temp = input().split(" ")
+			puz.append(temp)
+		return puz
+	
+	def f(self,start,goal):
+		'''Heuristic function to calculate heuristic value f(x) = h(x) + g(x)'''
+		return self.h(start.data, goal) + start.level
+
+	def h(self,start,goal):
+		'''Calculates the difference between the given puzzles'''
+		temp = 0
+		for i in range(self.n):
+			for j in range(self.n):
+				if start[i][j] != goal[i][j] and start[i][j] != '_':
+					temp += 1
+		return temp
+	
+	def process(self):
+		'''Accept Start and Goal Puzzle state'''
+		print('Enter the start state matrix\n')
+		start = self.accept()
+		print('Enter the goal state matrix\n')
+		goal = self.accept()	
+		start = Node(start,0,0)
+		start.fval = self.f(start, goal)
+		'''Put the start node in the open list'''
+		self.open.append(start)
+		print('\n\n')
+		while True:
+			cur = self.open[0]
+			print('')
+			print('  |')
+			print('  |')
+			print(" \\\'/\n")
+			for i in cur.data:
+				for j in i:
+					print(j, end = " ")
+				print()
+			'''If the difference between current and goal node is 0 we have reached our goal state'''
+			if(self.h(cur.data, goal) == 0):
+				break
+			for i in cur.generate_child():
+				i.fval = self.f(i,goal)
+				self.open.append(i)
+			self.closed.append(cur)
+			del self.open[0]
+		
+			'''sort the open list based on f value'''
+			self.open.sort(key = lambda x:x.fval, reverse = False)
+
+puz = Puzzle(3)
+puz.process()
